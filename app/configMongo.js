087ff02310665cc0e562dc1d907cc4141bfdde00
Hack Reactor Students <accounts@hackreactor.com>
2016-11-30 19:59:59 -0800

    Create MongoDB database

--- app/configMongo.js
+++ app/configMongo.js
@@ -1,4 +1,46 @@
 var mongoose = require('mongoose');
 var path = require('path');
 
-mongoose.connect('mongodb' + path.join(__dirname, '../db'));
+mongoose.connect('mongodb://localhost/app/db');
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  username: String,
+  password: String,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+
+var linkSchema = new Schema({
+  url: String,
+  shortUrl: String,
+  code: String,
+  title: String,
+  visits: Number,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+userSchema.pre('save', function(next) {
+  this.updatedAt = new Date();
+  if (!this.createdAt) {
+    this.createdAt = new Date();
+  }
+  next();
+});
+
+linkSchema.pre('save', function(next) {
+  this.updatedAt = new Date();
+  if (!this.createdAt) {
+    this.createdAt = new Date();
+  }
+  next();
+});
+
+var User = mongoose.model('User', userSchema);
+var Link = mongoose.model('Link', linkSchema);
+
+exports.modules.User = User;
+exports.modules.Link = Link;
